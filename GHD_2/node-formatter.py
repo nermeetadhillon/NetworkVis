348a02658f80d0484f81ee3aa79da66f18b78520
ndhillonwfb <136938826+ndhillonwfb@users.noreply.github.com>
2023-10-27 15:37:10 -0700

    GHD MaP 3

--- GHD_2/node-formatter.py
+++ GHD_2/node-formatter.py
@@ -7,7 +7,7 @@ import numpy as np
 import random
 
 
-ghdbiomed= pickle.load(open('GHD_Tokens_18Clusters_ForNodeReformatting.pickle','rb'))
+ghdbiomed= pickle.load(open('researchcluster_tokens.pkl','rb'))
 #colors = [(128,0,0),(165,42,42),(255,0,0),(255,215,0),(238,232,170),(255,255,0),(154,205,50),(127,255,0),(0,128,0),(143,188,143),(102,205,170),(47,79,79),(0,128,128),(0,255,255),(224,255,255),(64,224,208),(127,255,212),(70,130,180),(100,149,237),(0,191,255),(30,144,255),(135,206,250),(0,0,128),(0,0,255),(138,43,226),(75,0,130),(123,104,238),(139,0,139),(148,0,211),(186,85,211),(221,160,221),(238,130,238),(255,0,255),(199,21,133),(255,20,147),(255,192,203),(255,228,196),(139,69,19),(244,164,96),(188,143,143),(255,240,245),(245,255,250),(112,128,144),(240,255,240)]
 colors = pickle.load(open('RGBColors.pickle','rb'))
 
@@ -19,6 +19,7 @@ for i in range(len(ghdbiomed)):
     for j in ghdbiomed[i]:
         tokens.append(j)
     tokenslist.append(tokens)
+tokenslist.pop(-1)
 
 data = json.load(open('data.json','r'))
 json.dump(data,open('data.json_bak','w'))
@@ -35,7 +36,7 @@ for c,dictionary in enumerate(data['nodes']):
             data['nodes'][c].update(size_)
             data['nodes'][c].update(color_change)
             #print(color_change)
-        elif 'Global Health Disparities Research Cluster ' + str(i+1)  in dictionary['label']:
+        elif 'Research Cluster ' + str(i+1)  in dictionary['label']:
             colorstr = str(colors[i])
             color_change = { 'color' : 'rgb'+colorstr }
             size_ = {'size': 3.0 }

8aab83f4073ef49ee8b3280d6a032a2c11e383cb
ndhillonwfb <136938826+ndhillonwfb@users.noreply.github.com>
2023-11-07 12:12:14 -0800

    SDSU HLink overlayed on GHD Map 5

---
+++ GHD_SDSUHLink_4/tokens.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pickle5 as pickle
+
+df = pd.read_csv('231107_0.3_0.005_GHD_HLink_Overlay.csv')
+titles = df.columns.tolist()
+sublists = [titles[i:i + 30] for i in range(1, len(titles), 30)]
+
+filename = 'all_sublists.pkl'
+
+with open(filename, 'wb') as file:
+    pickle.dump(sublists, file)

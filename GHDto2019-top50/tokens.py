b03ee68f9baebe8750975896b85db94f989bc72e
nermeetadhillon <87878726+nermeetadhillon@users.noreply.github.com>
2024-04-26 02:16:02 -0700

    Add files via upload

---
+++ GHDto2019-top50/tokens.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pickle
+
+df = pd.read_csv('042624-GlobalHealthDisparities-Top50.csv')
+titles = df.columns.tolist()
+sublists = [titles[i:i + 30] for i in range(1, len(titles), 30)]
+
+filename = 'all_sublists.pkl'
+
+with open(filename, 'wb') as file:
+    pickle.dump(sublists, file)
